6302334397343104587380402dc3554c94b43b81
xavier facqueur <facqueur.xavier@gmail.com>
2023-01-19 19:53:12 +0100

    Add: project UI dependencies

---
+++ front-end/components/HomeSection.tsx
@@ -0,0 +1,10 @@
+import { Box, Button } from '@chakra-ui/react';
+import React from 'react';
+
+export const HomeSection = () => {
+  return (
+    <Box>
+      <h1>Home Section</h1>
+    </Box>
+  );
+};
